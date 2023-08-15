fbdd41070a398402e62a2fea47b508ce64b13de0
Ujjwal Gupta <guptaxujjwal@gmail.com>
2023-08-15 20:37:19 +0530

    add

--- starter/script.js
+++ starter/script.js
@@ -5,3 +5,4 @@
 //? Document Object Model (DOM):
 //* It is a Structured Representation of HTML Documents.
 //* It allows JavaScript to access HTML elements and STYLES to Manipulate them.
+//* It is a connection between JavaScript & HTML.
