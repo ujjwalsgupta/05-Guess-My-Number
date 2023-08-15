097ae6b446da500f6fd0e31837a68e9113ada01e
Ujjwal Gupta <guptaxujjwal@gmail.com>
2023-08-15 21:01:02 +0530

    add

--- starter/script.js
+++ starter/script.js
@@ -6,3 +6,11 @@
 //* It is a Structured Representation of HTML Documents.
 //* It allows JavaScript to access HTML elements and STYLES to Manipulate them.
 //* It is a connection between JavaScript & HTML.
+
+//? DOCUMENT:
+//* Document serves as an entry point for the web page's content, that is DOM tree.
+//* It is an object.
+
+//? querySelector():
+//* querySelector() is a Document object's method that returns the FIRST ELEMENT within the document that matches the specified selector, or group of selectors.
+//* If no matches are found, null is returned.
