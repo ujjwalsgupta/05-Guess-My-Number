8716fcb9669c80c033a20f5cdfdcf94dd03b5404
Ujjwal Gupta <guptaxujjwal@gmail.com>
2023-08-15 21:04:35 +0530

    add

--- starter/script.js
+++ starter/script.js
@@ -2,6 +2,8 @@
 
 //todo: What's the DOM and DOM Manipulation
 
+document.querySelector(".message").textContent;
+
 //? Document Object Model (DOM):
 //* It is a Structured Representation of HTML Documents.
 //* It allows JavaScript to access HTML elements and STYLES to Manipulate them.
@@ -11,6 +13,9 @@
 //* Document serves as an entry point for the web page's content, that is DOM tree.
 //* It is an object.
 
-//? querySelector():
+//? .querySelector():
 //* querySelector() is a Document object's method that returns the FIRST ELEMENT within the document that matches the specified selector, or group of selectors.
 //* If no matches are found, null is returned.
+
+//? .textContent():
+//* .textContent is used to select the text inside a selected element.
