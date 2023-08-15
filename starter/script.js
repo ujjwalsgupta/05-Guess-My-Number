e8e9c03eb251037968281908024edcb3a802596b
Ujjwal Gupta <guptaxujjwal@gmail.com>
2023-08-15 20:31:47 +0530

    add

---
+++ starter/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//todo: What's the DOM and DOM Manipulation
+
+//? Document Object Model (DOM):
+//* It is a Structured Representation of HTML Documents.
+//* It allows JavaScript to access HTML elements and STYLES to Manipulate them.
