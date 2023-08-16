ab208a6e844da19e9282b1fc8f54322434392fea
Ujjwal Gupta <guptaxujjwal@gmail.com>
2023-08-16 06:41:45 +0530

    sg

--- starter/main-script.js
+++ starter/main-script.js
@@ -1,6 +1,12 @@
 "use-strict";
 
 document.querySelector(".check").addEventListener("click", function () {
-  console.log(document.querySelector(".guess").value);
-  document.querySelector(".message").textContent = "🎉 Correct Number!";
+  const inputValue = Number(document.querySelector(".guess").value);
+  console.log(inputValue, typeof inputValue);
+
+  if (!inputValue) {
+    // inputValue if nothing is inputted = 0 in console (0 is a falsy value).
+    // Now in if-else, number -> boolean, inputValue is falsy, in order to let "if statement" work, inputValue should be truthy, so !inputValue.
+    document.querySelector(".message").textContent = "⛔ No Number";
+  }
 });
